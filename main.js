09340ad25ab44142ea6c5f92abbdc92ebe9005ea
amyhenning <henning.amy@gmail.com>
2018-11-30 01:51:39 +0000

    Cleaned up style.css and added notes to main.js

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-const originalButton = document.getElementById("original-button");
-const mysteryButton = document.getElementById("mystery-button");
+// Get the HTML element with the id "click-button"
+const clickButton = document.getElementById("click-button");
 
-// This random number function that will creates color codes for the randomColor variable
+// Get the HTML element with the id "scroll-button" 
+const scrollButton = document.getElementById("scroll-button");
+
+// This random number function creates color codes for the randomColor variable
 function rgb(num) {
   return Math.floor(Math.random() * num)
 }
 
+// The two functions below are separate so each button's new background color rgb code can be displayed
 function clickChange() {
 	const randomColor = 'rgb(' + rgb(255) + ',' + rgb(255) + ',' + rgb(255) + ')';
   event.target.style.backgroundColor = randomColor;
@@ -18,5 +22,5 @@ function scrollChange() {
   document.getElementById("scroll-color").innerHTML = "This color's rgb code is " + randomColor;
 }
 
-originalButton.onclick = clickChange;
-mysteryButton.onwheel = scrollChange;
+clickButton.onclick = clickChange;
+scrollButton.onwheel = scrollChange;
